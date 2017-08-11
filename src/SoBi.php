224fbc7c08ce697564cb64a14788aa0115074d4a
Tomasz Baranski <tomaszbe@hotmail.com>
2017-08-11 03:45:58 +0200

    set up autoload and create main class

---
+++ src/SoBi.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tomaszbe;
+
+/**
+* This class is responsible for communication with SocialBicycles API
+*/
+class SoBi
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
